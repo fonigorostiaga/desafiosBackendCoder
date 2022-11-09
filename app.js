2487dc097aba62624c14fa77be62bab1ca45cf88
Alfonso Gorostiaga <foni.gorostiaga@gmail.com>
2022-11-09 20:57:05 -0300

    websockets 2

--- app.js
+++ app.js
@@ -11,40 +11,59 @@ const {Server:HttpServer}=require('http')
 const http=new HttpServer(app)
 const io=new IoServer(http)
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
+
 
 app.use(express.static(__dirname+'/public'))
 
 app.use('/api',indexRouter)
+app.get('/health',(_req,res)=>{
+    res.status(200).json({
+        health:"up",
+        success:true,
+        environment:process.env.ENVIRONMENT
+    })
+})
 
 app.get('/',(_req,res)=>{
 
 
     res.sendFile('index',{root:__dirname})
 })
+const messages=[]
 io.on('connection',async(socket)=>{
     console.info("nuevo cliente conectado")
+    
+    socket.on('NEW_MESSAGE_TO_SERVER',data=>{
+        messages.push(data)
+        io.sockets.emit('NEW_MESSAGE_FROM_SERVER',data)
+    })
     const productos=await fileproductos.getAll()
-    socket.emit('UPDATE_DATA',productos)
+    socket.emit('UPDATE_DATA',{productos,messages})
+    socket.on('NEW_PRODUCT_TO_SERVER',async (data)=>{
+        console.log(data)
+        await fileproductos.save(data)
+        nuevosProductos=await fileproductos.getAll()
+        io.sockets.emit('NEW_PRODUCTS_FROM_SERVER',nuevosProductos)
+    })
 })
 
 
+// app.set('views', './views')
+// app.set('view engine', 'ejs')
+// app.post('/productos',(req,res)=>{
+//     const { title, thumbnail, price}=req.body
+//     fileproductos.save({title, thumbnail, price})
+//     res.redirect('/')
 
-app.post('/productos',(req,res)=>{
-    const { title, thumbnail, price}=req.body
-    fileproductos.save({title, thumbnail, price})
-    res.redirect('/')
+// })
 
-})
-
-app.get('/productos',async(_req,res)=>{
-    const productos=await fileproductos.getAll()
-    res.render('pages/verProductos', {productos:productos})
-})
-app.get('/verProductos',(_req,res)=>{
-    res.redirect('/')
-})
+// app.get('/productos',async(_req,res)=>{
+//     const productos=await fileproductos.getAll()
+//     res.render('pages/verProductos', {productos:productos})
+// })
+// app.get('/verProductos',(_req,res)=>{
+//     res.redirect('/')
+// })
 
 
 
--- public/main.js
+++ public/main.js
@@ -1,7 +1,15 @@
 const socket=io()
 
-let productos=[]
+let messages=[];
 
+let productos=[]
+const updateMessages=(data)=>{
+    messageToHtml=''
+    data.forEach(i=>{
+        messageToHtml=messageToHtml+`<li><span class="correo"><b>${i.email}</b></span> <span class="timeStamp">[${i.timeStamp}]</span>: <span class="message">${i.message}</span></li>`
+    })
+    document.querySelector("#messagesList").innerHTML=messageToHtml
+}
 
   const updateProductos=(data)=>{
     productosToHtml=''
@@ -12,7 +20,58 @@ let productos=[]
   }
 
 socket.on('UPDATE_DATA',data=>{
-    console.log(data)
-    productos=data
+    console.log(data.productos)
+    messages=data.messages
+    productos=data.productos
     updateProductos(productos)
+    updateMessages(messages)
 })
+const sendNewProduct=()=>{
+    const title=document.querySelector("#title").value
+    const thumbnail=document.querySelector("#thumbnail").value
+    const price=document.querySelector("#price").value
+    if(!title||!thumbnail||!price){
+        alert("te faltan datos")
+        return
+    }
+    const nuevoProducto={
+        title, thumbnail,price
+    }
+    socket.emit('NEW_PRODUCT_TO_SERVER',nuevoProducto)
+    document.querySelector("#title").value=''
+    document.querySelector("#thumbnail").value=''
+    document.querySelector("#price").value=''
+
+}
+socket.on('NEW_PRODUCTS_FROM_SERVER',data=>{
+    updateProductos(data)
+})
+
+
+socket.on('UPDATE_MESSAGES',data=>{
+    messages=data;
+    updateMessages(messages)
+})
+const sendNewMessage=()=>{
+
+    const timeStamp=new Date().toLocaleString()
+    const email=document.querySelector("#userEmail").value
+    const message=document.querySelector("#mensaje").value
+    if(!email||!message){
+        alert("faltan datos")
+        return
+    }
+    const messageObj={
+        timeStamp,
+        email,
+        message
+    }
+    socket.emit('NEW_MESSAGE_TO_SERVER',messageObj)
+    document.querySelector("#mensaje").value=''
+    
+}
+socket.on('NEW_MESSAGE_FROM_SERVER',data=>{
+    messages.push(data)
+    updateMessages(messages)
+})
+
